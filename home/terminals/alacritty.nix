0b69a618bfdf222ac37fe3929ee1c5b6801b403f
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-21 18:53:04 -0600

    refactor(alacritty): move catppuccin

--- home/terminals/alacritty.nix
+++ home/terminals/alacritty.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: let
+  catppuccin = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/catppuccin/alacritty/3c808cbb4f9c87be43ba5241bc57373c793d2f17/catppuccin-mocha.yml";
+    hash = "sha256-28Tvtf8A/rx40J9PKXH6NL3h/OKfn3TQT1K9G8iWCkM=";
+  };
   theme = import ../lib/theme {};
   inherit (theme.terminal) font fontItalic size opacity;
 in {
@@ -13,12 +17,7 @@ in {
         bold_italic.family = fontItalic;
       };
 
-      imports = [
-        (pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/catppuccin/alacritty/3c808cbb4f9c87be43ba5241bc57373c793d2f17/catppuccin-mocha.yml";
-          hash = "sha256-28Tvtf8A/rx40J9PKXH6NL3h/OKfn3TQT1K9G8iWCkM=";
-        })
-      ];
+      imports = [catppuccin];
     };
   };
 }
