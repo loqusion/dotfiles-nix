c4a8217efed14f685ffd9d2acb9dfdcb1b8a4435
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-12-21 22:06:34 -0600

    feat(foot): finally get theme import to work

--- home/profiles/kuro/default.nix
+++ home/profiles/kuro/default.nix
@@ -3,7 +3,7 @@
     # ./xresources.nix
     ../../editors/helix
     ../../programs
-    # ../../terminals/foot.nix
+    ../../terminals/foot.nix
     # ../../terminals/alacritty.nix
     # ../../terminals/wezterm.nix
   ];
--- home/terminals/foot.nix
+++ home/terminals/foot.nix
@@ -1,40 +1,37 @@
-{
-  pkgs,
-  lib,
-  ...
-}:
-with lib; let
-  # TODO: This isn't working
+{pkgs, ...}: let
   catppuccinDrv = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/loqusion/catppuccin-foot/3df09b27935521bf71489821965c2fe0c2c8d672/catppuccin-mocha.ini";
+    url = "https://raw.githubusercontent.com/catppuccin/foot/009cd57bd3491c65bb718a269951719f94224eb7/catppuccin-mocha.conf";
     hash = "sha256-plQ6Vge6DDLj7cBID+DRNv4b8ysadU2Lnyeemus9nx8=";
   };
   theme = import ../lib/theme {};
   inherit (theme.terminal) font fontItalic size opacity;
-  catppuccin = builtins.fromTOML (builtins.readFile catppuccinDrv);
 in {
   programs.foot = {
     enable = true;
-    settings =
-      {
-        main = {
-          font = "${font}:size=${size}";
-          font-italic = "${fontItalic}:size=${size}";
-        };
+    settings = {
+      main = let
+        withSize = "size=${toString size}";
+      in {
+        font = "${font}:${withSize}";
+        font-bold = "${font}:style=Bold:${withSize}";
+        font-italic = "${fontItalic}:style=Italic:${withSize}";
+        font-bold-italic = "${fontItalic}:style=BoldItalic:${withSize}";
+        box-drawings-uses-font-glyphs = true;
+        include = "${catppuccinDrv}";
+      };
 
-        scrollback = {
-          lines = 10000;
-        };
+      scrollback = {
+        lines = 10000;
+      };
 
-        url = {
-          launch = "xdg-open \${url}";
-          protocols = "http, https, ftp, ftps, file";
-        };
+      url = {
+        launch = "xdg-open \${url}";
+        protocols = "http, https, ftp, ftps, file";
+      };
 
-        # colors = {
-        #   alpha = opacity;
-        # };
-      }
-      // (debug.traceVal catppuccin);
+      colors = {
+        alpha = opacity;
+      };
+    };
   };
 }
